e217334cd12cd9eec04419c1906c65d9ceb65659
Enzo Pages <Enzo.Pages@etu.univ-smb.fr>
2025-06-10 08:45:13 +0200

    1reiyfiyefef

---
+++ SIBILIATP11/UserControl/CommandeARestituer.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SIBILIATP11.UserControl
+{
+    /// <summary>
+    /// Logique d'interaction pour CommandeARestituer.xaml
+    /// </summary>
+    public partial class CommandeARestituer : UserControl
+    {
+        public CommandeARestituer()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SIBILIATP11/Windows/WindowConnexion.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SIBILIATP11.Windows
+{
+    /// <summary>
+    /// Logique d'interaction pour WindowConnexion.xaml
+    /// </summary>
+    public partial class WindowConnexion : Window
+    {
+        public WindowConnexion()
+        {
+            InitializeComponent();
+        }
+    }
+}
